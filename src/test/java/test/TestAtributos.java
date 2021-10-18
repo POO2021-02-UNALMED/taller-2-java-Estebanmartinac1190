bae25b7d92e7acc30a8d4df09c7381e13e352a55
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-10-17 20:10:20 -0500

    Update TestAtributos.java

--- src/test/java/test/TestAtributos.java
+++ src/test/java/test/TestAtributos.java
@@ -34,6 +34,94 @@ public class TestAtributos {
 	@Test
    	public void testAtributosAuto(){
 		
+package test;
+
+public class Asiento {
+    // ATRIBUTOS
+    public String color;
+    public int precio;
+    public int registro;
+
+    public void setcambiarColor(String cambio) {
+        if (cambio == "rojo" || cambio == "verde" || cambio == "amarillo" || cambio == "negro" || cambio == "blanco") {
+            this.color = cambio;
+        }
+
+    }
+
+}
+
+		package test;
+
+public class Auto {
+    // Atributos
+    public String modelo;
+    public int precio;
+    public Array Asientos[];
+    public String marca;
+    public Motor motor;
+    public int registro;
+    public static int cantidadCreados;
+
+    // MÉTODOS
+
+    public int getcantidadAsiento() {
+        int loca;
+        for (int i = 0; Asientos.length; i++) {
+            if (Asientos[i] != null) {
+                loca++;
+            }
+        }
+        return loca;
+    }
+
+    public String getverificarIntegridad( ){
+        boolean k1,k2;
+        for(inti=0;Asientos.length;i++){
+            if (Asiento[i]!=null){
+                if(motor.registro==Asiento[i].registro) && (motor.registro==auto.registro){
+                    k1=true;
+                    k2=false;
+                }
+                else{
+                    k2=true;
+                    k1=false;
+                }
+            }
+        }
+        if (k1==true){
+            System.out.println("Auto original");
+        }
+        else{
+            System.out.println("Las piezas no son originales");
+        }
+
+    }
+
+}
+		package test;
+
+public class Motor {
+    // ATRIBUTOS
+    public int numeroCilindros;
+    public String tipo;
+    public int registro;
+
+    // MÉTODOS
+
+    public void setcambiarRegistro(int regist) {
+        this.registro = regist;
+    }
+
+    public void setasignarTipo(String tpm) {
+        if (tpm == "electrico" || tpm == "gasolina") {
+            this.tipo = tpm;
+        }
+
+    }
+
+}
+		
 		Auto v = new Auto();
 		v.asientos = new Asiento[5];
 		v.marca = "tesla";
